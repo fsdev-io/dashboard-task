96af5bbcc469c9127b776deb3d57b17a07390f8f
moczydlowski.kamil@gmail.com <moczydlowski.kamil@gmail.com>
2021-01-18 23:19:42 +0100

    lint

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,10 +1,41 @@
 import React from "react";
-import DashboardComponent from "./Dashboard";
+import { Row } from "antd";
+import styled from "styled-components";
+import Card from "./Card";
 
-const Dashboard = () => {
-  const props = {};
+import money from "../../icons/money.svg";
+import users from "../../icons/users.svg";
+import sale from "../../icons/sale.svg";
+import complaint from "../../icons/complaint.svg";
+import Production from "./Production";
+import GeoMap from "./GeoMap";
 
-  return <DashboardComponent {...props} />;
-};
+const Container = styled.div`
+  padding: 16px;
+`;
 
-export default Dashboard;
+const Title = styled.h1`
+  margin: 12px 0 18px 20px;
+`;
+
+const StyledRow = styled(Row)`
+  margin-bottom: 16px;
+`;
+
+const DashboardComponent = () => (
+  <Container>
+    <Title>React Dashboard</Title>
+    <StyledRow gutter={[16, 16]}>
+      <Card name="Total Profit" type="profit" icon={money} prefix="$" />
+      <Card name="Active Users" type="users" icon={users} />
+      <Card name="New Orders" type="orders" icon={sale} />
+      <Card name="Open Complaints" type="complaints" icon={complaint} />
+    </StyledRow>
+    <Row gutter={[16, 16]}>
+      <Production />
+      <GeoMap />
+    </Row>
+  </Container>
+);
+
+export default DashboardComponent;
